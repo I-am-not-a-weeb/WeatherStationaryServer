4dbff4eabb481a60226b74062e47c51e93071bb2
I_am_not_a_weeb <kubutek04@gmail.com>
2024-01-28 16:35:32 +0100

    przedostatnie fixy

--- ProjektSerwer.cpp
+++ ProjektSerwer.cpp
@@ -33,6 +33,12 @@ std::vector <utility::string_t> auth_vec = {};
 void handle_get(web::http::http_request request);
 void handle_patch(web::http::http_request request);
 void handle_delete(web::http::http_request request);
+void handle_post(web::http::http_request request);
+
+struct worker{ std::wstring name; std::wstring surname; };
+
+std::vector< worker > workers_vec;
+std::vector<std::wstring> items_vec, orders_vec;
 
 struct struct_date
 {
@@ -207,6 +213,7 @@ int main(int argc, char* argv[])
 	}
 
     web::http::experimental::listener::http_listener listener(server_url);
+    
     listener.support(web::http::methods::GET, std::bind(handle_get,std::placeholders::_1));
     listener.support(web::http::methods::PATCH, std::bind(handle_patch, std::placeholders::_1));
     bool listenening = false;
@@ -331,8 +338,6 @@ void handle_get(web::http::http_request request)
                 {
                     std::stringstream tmp_ss;
 
-                    web::json::object json_query_filter = web::json::value::parse(query[L"filter"]).as_object();
-
                     utility::string_t time_greaterthan{ L"00:00:00" }, time_lessthan{ L"24:00:00" };
                     double temp_greaterthan{ 0 }, temp_lessthan{ 101 };
                     double humi_greaterthan{ 0 }, humi_lessthan{ 101 };
@@ -340,94 +345,97 @@ void handle_get(web::http::http_request request)
                     double lux_greaterthan{ 0 }, lux_lessthan{ 10001 };
                     double rpm_greaterthan{0 }, rpm_lessthan{ 10001 };
 
-
-                    if(json_query_filter[L"time"].is_object())
+                    if (query.contains(L"filter"))
                     {
-                    	web::json::object json_query_filter_time = json_query_filter[L"time"].as_object();
+                        web::json::object json_query_filter = web::json::value::parse(query[L"filter"]).as_object();
 
-                        if(json_query_filter_time[L"gt"].is_string())
+                        if (json_query_filter[L"time"].is_object())
                         {
-                            time_greaterthan = json_query_filter_time[L"gt"].as_string();
+                            web::json::object json_query_filter_time = json_query_filter[L"time"].as_object();
+
+                            if (json_query_filter_time[L"gt"].is_string())
+                            {
+                                time_greaterthan = json_query_filter_time[L"gt"].as_string();
+                            }
+
+                            if (json_query_filter_time[L"lt"].is_string())
+                            {
+                                time_lessthan = json_query_filter_time[L"lt"].as_string();
+                            }
                         }
 
-                    	if(json_query_filter_time[L"lt"].is_string())
-                    	{
-                            time_lessthan = json_query_filter_time[L"lt"].as_string();
-                    	}
-                    }
-
-                    if(json_query_filter[L"temp"].is_object())
-                    {
-                    	web::json::object json_query_filter_temp = json_query_filter[L"temp"].as_object();
-
-						if(json_query_filter_temp[L"gt"].is_string())
-						{
-							temp_greaterthan = stod(json_query_filter_temp[L"gt"].as_string());
-						}
-
-						if(json_query_filter_temp[L"lt"].is_string())
-						{
-							temp_lessthan = stod(json_query_filter_temp[L"lt"].as_string());
-						}
-					}
-
-                    if(json_query_filter[L"humi"].is_object())
-                    {
-                    	web::json::object json_query_filter_humi = json_query_filter[L"humi"].as_object();
-
-                        if(json_query_filter_humi[L"gt"].is_string())
-						{
-							humi_greaterthan = stod(json_query_filter_humi[L"gt"].as_string());
-						}
-
-						if(json_query_filter_humi[L"lt"].is_string())
-						{
-							humi_lessthan = stod(json_query_filter_humi[L"lt"].as_string());
-						}
-                    }
-
-                    if(json_query_filter[L"ppm"].is_object())
-                    {
-                    	web::json::object json_query_filter_ppm = json_query_filter[L"ppm"].as_object();
-
-						if(json_query_filter_ppm[L"gt"].is_string())
-						{
-							ppm_greaterthan = stod(json_query_filter_ppm[L"gt"].as_string());
-						}
-                        if(json_query_filter_ppm[L"lt"].is_string())
+                        if (json_query_filter[L"temp"].is_object())
                         {
-                            ppm_lessthan = stod(json_query_filter_ppm[L"lt"].as_string());
+                            web::json::object json_query_filter_temp = json_query_filter[L"temp"].as_object();
+
+                            if (json_query_filter_temp[L"gt"].is_string())
+                            {
+                                temp_greaterthan = stod(json_query_filter_temp[L"gt"].as_string());
+                            }
+
+                            if (json_query_filter_temp[L"lt"].is_string())
+                            {
+                                temp_lessthan = stod(json_query_filter_temp[L"lt"].as_string());
+                            }
+                        }
+
+                        if (json_query_filter[L"humi"].is_object())
+                        {
+                            web::json::object json_query_filter_humi = json_query_filter[L"humi"].as_object();
+
+                            if (json_query_filter_humi[L"gt"].is_string())
+                            {
+                                humi_greaterthan = stod(json_query_filter_humi[L"gt"].as_string());
+                            }
+
+                            if (json_query_filter_humi[L"lt"].is_string())
+                            {
+                                humi_lessthan = stod(json_query_filter_humi[L"lt"].as_string());
+                            }
+                        }
+
+                        if (json_query_filter[L"ppm"].is_object())
+                        {
+                            web::json::object json_query_filter_ppm = json_query_filter[L"ppm"].as_object();
+
+                            if (json_query_filter_ppm[L"gt"].is_string())
+                            {
+                                ppm_greaterthan = stod(json_query_filter_ppm[L"gt"].as_string());
+                            }
+                            if (json_query_filter_ppm[L"lt"].is_string())
+                            {
+                                ppm_lessthan = stod(json_query_filter_ppm[L"lt"].as_string());
+                            }
+                        }
+
+                        if (json_query_filter[L"lux"].is_object())
+                        {
+                            web::json::object json_query_filter_lux = json_query_filter[L"lux"].as_object();
+
+                            if (json_query_filter_lux[L"gt"].is_string())
+                            {
+                                lux_greaterthan = stod(json_query_filter_lux[L"gt"].as_string());
+                            }
+                            if (json_query_filter_lux[L"lt"].is_string())
+                            {
+                                lux_lessthan = stod(json_query_filter_lux[L"lt"].as_string());
+                            }
+                        }
+
+                        if (json_query_filter[L"rpm"].is_object())
+                        {
+                            web::json::object json_query_filter_rpm = json_query_filter[L"rpm"].as_object();
+
+                            if (json_query_filter_rpm[L"gt"].is_string())
+                            {
+                                rpm_greaterthan = stod(json_query_filter_rpm[L"gt"].as_string());
+                            }
+                            if (json_query_filter_rpm[L"lt"].is_string())
+                            {
+                                rpm_lessthan = stod(json_query_filter_rpm[L"lt"].as_string());
+                            }
                         }
                     }
-
-                    if(json_query_filter[L"lux"].is_object())
-                    {
-                    	web::json::object json_query_filter_lux = json_query_filter[L"lux"].as_object();
-
-                        if (json_query_filter_lux[L"gt"].is_string())
-                        {
-                            lux_greaterthan = stod(json_query_filter_lux[L"gt"].as_string());
-                        }
-                        if (json_query_filter_lux[L"lt"].is_string())
-                        {
-                            lux_lessthan = stod(json_query_filter_lux[L"lt"].as_string());
-                        }
-                    }
-
-                    if(json_query_filter[L"rpm"].is_object())
-                    {
-                    	web::json::object json_query_filter_rpm = json_query_filter[L"rpm"].as_object();
-
-						if (json_query_filter_rpm[L"gt"].is_string())
-						{
-							rpm_greaterthan = stod(json_query_filter_rpm[L"gt"].as_string());
-						}
-						if (json_query_filter_rpm[L"lt"].is_string())
-						{
-							rpm_lessthan = stod(json_query_filter_rpm[L"lt"].as_string());
-						}
-					}
-
                     while (!file.eof())                     /// reading file line by line and comparing to filter query
                     {
                         std::string time;
@@ -457,6 +465,116 @@ void handle_get(web::http::http_request request)
                     return;
                 }
             }
+        }
+        else if(path[0] == L"workers")
+        {
+	        if(path.size()==1)
+	        {
+	        	web::json::value json_response;
+				web::json::value json_response_workers = web::json::value::array();
+
+				for(auto i : workers_vec)
+				{
+					web::json::value json_response_worker;
+					json_response_worker[L"name"] = web::json::value::string(i.name);
+					json_response_worker[L"surname"] = web::json::value::string(i.surname);
+
+					json_response_workers[json_response_workers.size()] = json_response_worker;
+				}
+				json_response[L"workers"] = json_response_workers;
+
+				request.reply(web::http::status_codes::OK, json_response);
+				return;
+			}
+			else if(path.size()==2)
+			{
+				web::json::value json_response;
+				web::json::value json_response_worker;
+
+				for(auto i : workers_vec)
+				{
+					if(i.surname == path[1])
+					{
+						json_response_worker[L"name"] = web::json::value::string(i.name);
+						json_response_worker[L"surname"] = web::json::value::string(i.surname);
+
+						json_response[L"worker"] = json_response_worker;
+
+						request.reply(web::http::status_codes::OK, json_response);
+						return;
+					}
+				}
+				request.reply(web::http::status_codes::NotFound);
+				return;
+			}
+		}
+		else if(path[0] == L"items")
+		{
+			if(path.size()==1)
+			{
+				web::json::value json_response;
+				web::json::value json_response_items = web::json::value::array();
+
+				for(auto i : items_vec)
+				{
+					json_response_items[json_response_items.size()] = web::json::value::string(i);
+				}
+				json_response[L"items"] = json_response_items;
+
+				request.reply(web::http::status_codes::OK, json_response);
+				return;
+			}
+			else if(path.size()==2)
+			{
+				web::json::value json_response;
+
+				for(auto i : items_vec)
+				{
+					if(i == path[1])
+					{
+						json_response[L"item"] = web::json::value::string(i);
+
+						request.reply(web::http::status_codes::OK, json_response);
+						return;
+					}
+				}
+				request.reply(web::http::status_codes::NotFound);
+				return;
+			}
+		}
+        else if(path[0]==L"orders")
+        {
+	        if(path.size()==1)
+	        {
+	        	web::json::value json_response;
+				web::json::value json_response_orders = web::json::value::array();
+
+				for(auto i : orders_vec)
+				{
+					json_response_orders[json_response_orders.size()] = web::json::value::string(i);
+				}
+				json_response[L"orders"] = json_response_orders;
+
+				request.reply(web::http::status_codes::OK, json_response);
+				return;
+			}
+			else if(path.size()==2)
+			{
+				web::json::value json_response;
+
+				for(auto i : orders_vec)
+				{
+					if(i == path[1])
+					{
+						json_response[L"order"] = web::json::value::string(i);
+
+						request.reply(web::http::status_codes::OK, json_response);
+						return;
+					}
+				}
+				request.reply(web::http::status_codes::NotFound);
+				return;
+			}
         }
         request.reply(web::http::status_codes::BadRequest);
     }
@@ -512,6 +630,70 @@ void handle_patch(web::http::http_request request)
 					});
             }
 		}
+        else if(path[0]==L"workers")
+        {
+	       web::json::value json_request{ request.extract_json().get() };
+
+			if (!json_request.has_field(L"name") || !json_request.has_field(L"surname") || !json_request.has_field(L"new_surname"))
+			{
+				request.reply(web::http::status_codes::BadRequest);
+				return;
+			}
+
+			for (auto i = workers_vec.begin(); i != workers_vec.end(); i++)
+			{
+				if (i->surname == json_request.at(L"surname").as_string())
+				{
+                    i->name = json_request.at(L"new_name").as_string();
+					i->surname = json_request.at(L"new_surname").as_string();
+
+					request.reply(web::http::status_codes::OK);
+					return;
+				}
+			}
+			request.reply(web::http::status_codes::NotFound);
+			return;
+		}
+		else if(path[0] == L"items")
+		{
+	        web::json::value json_request{ request.extract_json().get() };
+
+            if(!json_request.has_field(L"name"))
+            {
+	            request.reply(web::http::status_codes::BadRequest);
+            }
+
+            for(auto i = items_vec.begin(); i!=items_vec.end(); i++)
+            {
+	            if(*i == json_request.at(L"name").as_string())
+	            {
+	            	*i = json_request.at(L"new_name").as_string();
+
+					request.reply(web::http::status_codes::OK);
+					return;
+				}
+            }
+            request.reply(web::http::status_codes::NotFound);
+            return;
+        }
+        else if(path[0] == L"orders")
+        {
+        	web::json::value json_request{ request.extract_json().get() };
+
+            for(auto i = orders_vec.begin(); i!=orders_vec.end(); i++)
+            {
+                if(*i == json_request.at(L"order").as_string())
+                {
+	                *i = json_request.at(L"new_order").as_string();
+
+                    request.reply(web::http::status_codes::OK);
+                    return;
+                }
+            }
+            request.reply(web::http::status_codes::NotFound);
+            return;
+		}
+
         request.reply(web::http::status_codes::BadRequest);
 	}
 	catch(std::exception& e)
@@ -697,11 +879,154 @@ void handle_delete(web::http::http_request request)
                 }
             }
 		}
+        else if(path[0] == L"workers")
+        {
+            web::json::value json_request{ request.extract_json().get() };
+
+            if (!json_request.has_field(L"surname"))
+            {
+                request.reply(web::http::status_codes::BadRequest);
+                return;
+            }
+
+            for(auto i = workers_vec.begin(); i!=workers_vec.end(); i++)
+            {
+                if (i->surname == json_request.at(L"surname").as_string())
+                {
+                    workers_vec.erase(i);
+                    request.reply(web::http::status_codes::OK);
+                }
+			}
+            request.reply(web::http::status_codes::NotFound);
+            return;
+        }
+        else if(path[0] == L"items")
+        {
+	        web::json::value json_request{ request.extract_json().get() };
+
+			if (!json_request.has_field(L"item_name"))
+			{
+				request.reply(web::http::status_codes::BadRequest);
+				return;
+			}
+
+			for (auto i = items_vec.begin(); i != items_vec.end(); i++)
+			{
+				if (*i == json_request.at(L"item_name").as_string())
+				{
+					items_vec.erase(i);
+                    request.reply(web::http::status_codes::OK);
+				}
+			}
+            request.reply(web::http::status_codes::NotFound);
+			return;
+		}
+		else if(path[0] == L"orders")
+		{
+			web::json::value json_request{ request.extract_json().get() };
+
+			if (!json_request.has_field(L"order"))
+			{
+				request.reply(web::http::status_codes::BadRequest);
+				return;
+			}
+
+			for (auto i = orders_vec.begin(); i != orders_vec.end(); i++)
+			{
+				if (*i == json_request.at(L"order").as_string())
+				{
+					orders_vec.erase(i);
+                    request.reply(web::http::status_codes::OK);
+                    return;
+				}
+			}
+			request.reply(web::http::status_codes::NotFound);
+			return;
+		}
 		request.reply(web::http::status_codes::BadRequest);
 	}
 	catch(std::exception& e)
 	{
 		std::cerr << "Error: " << e.what() << std::endl;
 		request.reply(web::http::status_codes::NotAcceptable, e.what());
+	}
+}
+
+void handle_post(web::http::http_request request)
+{
+    try {
+
+        if (!request.headers().has(L"Authorization"))
+        {
+            request.reply(web::http::status_codes::Unauthorized, L"Unauthorized");
+            return;
+        }
+
+        bool authenticated = false;
+        for (auto i : auth_vec)
+        {
+            if (i == request.headers()[L"Authorization"].substr(6))
+            {
+                authenticated = true;
+                break;
+            }
+        }
+
+        if (!authenticated)
+        {
+            request.reply(web::http::status_codes::Unauthorized, L"Unauthorized");
+            return;
+        }
+
+        web::json::value json{ request.extract_json().get() };
+
+        const std::vector path{ web::http::uri::split_path(web::http::uri::decode(request.relative_uri().path())) };
+        if (path.empty())
+        {
+            request.reply(web::http::status_codes::BadRequest);
+            return;
+        }
+        else if (path[0] == L"workers")
+        {
+            if (!json.has_field(L"name") || !json.has_field(L"surname"))
+            {
+                request.reply(web::http::status_codes::BadRequest);
+                return;
+            }
+
+            workers_vec.push_back({ json.at(L"name").as_string(),json.at(L"surname").as_string() });
+            request.reply(web::http::status_codes::Created);
+        }
+        else if (path[0] == L"items")
+        {
+            if (!json.has_field(L"item_name"))
+            {
+                request.reply(web::http::status_codes::BadRequest);
+                return;
+            }
+
+            items_vec.push_back(json.at(L"item_name").as_string());
+            request.reply(web::http::status_codes::Created);
+        }
+        else if (path[0] == L"orders")
+        {
+            if (!json.has_field(L"order"))
+            {
+                request.reply(web::http::status_codes::BadRequest);
+                return;
+            }
+
+            orders_vec.push_back(json.at(L"order").as_string());
+            request.reply(web::http::status_codes::Created);
+        }
+        else
+        {
+            request.reply(web::http::status_codes::BadRequest);
+            return;
+        }
+    }
+	catch(std::exception& e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
 	}
 }
